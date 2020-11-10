e5119964ac077a1ad7704c70faa441fa15a28cdb
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2020-11-10 22:06:52 +0200

    Задача№1
    Решена задача №1

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,13 @@
 const itemRef = document.querySelectorAll('.item');
-console.log(itemRef)
+console.log(`В списке ${itemRef.length} категории`)
+const titelRef = document.querySelectorAll('h2');
+const appp = titelRef.forEach((value) => 
+{   const listRef = value.nextElementSibling
+    console.log(`Категория: ${value.textContent}`);
+    console.dir(`Количество элементов: ${listRef.childElementCount}`)
+    
+}) 
+
+
+
 
