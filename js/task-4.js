0aefb63cc0afdf2e39f11c12845f32305996affb
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2020-11-11 22:36:54 +0200

    fix

---
+++ js/task-4.js
@@ -0,0 +1,15 @@
+const increment = () =>{
+    value +=1
+} 
+
+const decrement = () =>{
+    value -=1
+} 
+
+const discRef = document.querySelector('button')
+const incrRef = document.querySelector('button')
+const counterValue  = document.querySelector('#value')
+
+console.log(discRef)
+console.log(incrRef)
+console.log(counterValue )
