3418dac651f185d80325b1d0f71190969ddf7a91
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2020-11-11 13:54:02 +0200

    задача №3
    Решина задача на 60%

---
+++ js/task-3.js
@@ -0,0 +1,33 @@
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const toImgGallery = document.querySelector('#gallery')
+const createImgCard = image => {
+    const imgList = document.createElement('li');
+    imgList.classList.add('img-lsit')
+    const toImgList = document.createElement('img');
+    toImgList.classList.add('img-products')
+    toImgList.src = images.url
+    console.log(toImgList.url)
+    toImgList.alt = images.alt
+    imgList.append(toImgList)
+    toImgGallery.append(imgList)
+    return toImgGallery;
+}
+console.log(images[0])
+console.log(createImgCard(images[0])) 
