0194c74930da23153fb4a5af3ced3c85afeaedc0
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2020-11-13 18:26:42 +0200

    дз7
    ДЗ готово на 100%

--- js/task-7.js
+++ js/task-7.js
@@ -1,15 +1,9 @@
 const inputRef = document.querySelector('#font-size-control')
 const textAreaRef = document.querySelector('#text')
 
-console.dir(inputRef)
-console.dir(textAreaRef.style.fontSize)
-textAreaRef.style.fontSize = 10
-console.dir(textAreaRef.style.fontSize)
-
-
 inputRef.addEventListener('change', e => {
-    textAreaRef.style.fontSize = Number(e.target.value)
-    console.log(e.target.value)
-    console.dir(textAreaRef.style.fontSize)
+
+textAreaRef.style.fontSize = `${e.target.value}px`
+   
 })
 
---
+++ js/task-8.js
@@ -0,0 +1,32 @@
+const inputRef = document.querySelector('input')
+const createRef = document.querySelector('button[data-action="render"]')
+const destroyRef = document.querySelector('button[data-action="destroy"]')
+const boxesRef = document.querySelector('#boxes')
+
+createRef.addEventListener("click", getAmount);
+destroyRef.addEventListener("click", destroyBoxes);
+
+function getAmount() {
+  let amount = +inputRef.value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  let basicSize = 30;
+  let newBox = document.createElement("div");
+  for (var i = 0; i < amount; i++) {
+    let size = basicSize + i * 10;
+    let div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+    newBox.appendChild(div);
+  }
+  boxes.appendChild(newBox);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+function random() {
+  return Math.floor(Math.random() * 256);
+}
